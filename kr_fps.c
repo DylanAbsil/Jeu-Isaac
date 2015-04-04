d40893042bf0c0cfc27a6b4269e90ed5793f7d8f
Oherrou <olivier.herrou@insa-rennes.fr>
2015-04-04 16:30:49 +0200

    Kr_Fps : Doxygen

--- kr_fps.c
+++ kr_fps.c
@@ -17,6 +17,18 @@
 
 #include "kr_fps.h"
 
+
+/*!
+*  \fn     Kr_Fps *Kr_Fps_Init(SDL_Renderer *pRenderer, TTF_Font *pFont, SDL_Rect *pRectPosition, SDL_Color color, Boolean bMustShow)
+*  \brief  Function to initialize a Kr_Fps structure
+*
+*  \param  pRenderer	 a pointer to the renderer
+*  \param  pFont		 a pointer to a font
+*  \param  pRectPosition a pointer to the position of the text
+*  \param  color		 the color of the text
+*  \param  bMustShow	 True if we must show the fps
+*  \return the initialize structure if everything is ok, NULL otherwise
+*/
 Kr_Fps *Kr_Fps_Init(SDL_Renderer *pRenderer, TTF_Font *pFont, SDL_Rect *pRectPosition, SDL_Color color, Boolean bMustShow)
 {
 	Kr_Fps *pFPS = NULL;
@@ -34,7 +46,17 @@ Kr_Fps *Kr_Fps_Init(SDL_Renderer *pRenderer, TTF_Font *pFont, SDL_Rect *pRectPos
 
 
 
-void    Kr_Fps_Wait(Kr_Fps *pFPS, Uint32 *iCurrentTime, Uint32 *iPreviousTime, Uint32 iFPS)
+/*!
+*  \fn     void Kr_Fps_Wait(Kr_Fps *pFPS, Uint32 *iCurrentTime, Uint32 *iPreviousTime, Uint32 iFPS)
+*  \brief  Function to handle the FPS 
+*
+*  \param  pFPS			 a pointer to the Kr_Fps Structure
+*  \param  iCurrentTime	 the current time of the exe
+*  \param  iPreviousTime the previous time of the exe
+*  \param  iFPS			 the FPS we want
+*  \return none
+*/
+void Kr_Fps_Wait(Kr_Fps *pFPS, Uint32 *iCurrentTime, Uint32 *iPreviousTime, Uint32 iFPS)
 {
 	char szFPS[30] = "";
 
@@ -55,10 +77,18 @@ void    Kr_Fps_Wait(Kr_Fps *pFPS, Uint32 *iCurrentTime, Uint32 *iPreviousTime, U
 		UTIL_FreeTexture(&pFPS->pTexture);
 		pFPS->pTexture = Kr_Text_FontCreateTexture(pFPS->pRenderer, pFPS->pFont, szFPS, pFPS->color, TRUE, pFPS->pRectPosition); 
 		return;
-	}
-	
+	}	
 }
 
+
+
+/*!
+*  \fn     void Kr_FPS_Show(Kr_Fps *pFPS)
+*  \brief  Function to Print the FPS
+*
+*  \param  pFPS			 a pointer to the Kr_Fps Structure
+*  \return none
+*/
 void Kr_FPS_Show(Kr_Fps *pFPS)
 {
 	if (pFPS->bMustShow)
@@ -66,18 +96,3 @@ void Kr_FPS_Show(Kr_Fps *pFPS)
 		SDL_RenderCopy(pFPS->pRenderer, pFPS->pTexture, NULL, pFPS->pRectPosition); // affichage des FPS
 	}
 }
-/*
-iCurrentTime = SDL_GetTicks();
-if (iCurrentTime - iPreviousTime > (1000 / KR_FPS))
-{
-sprintf(szFPS, "FPS : %3.0lf", (1.0 / (iCurrentTime - iPreviousTime) * 1000));
-iPreviousTime = iCurrentTime;
-}
-else
-{
-//Kr_Log_Print(KR_LOG_INFO, "Attente de %d \n", (1000 / KR_FPS) - (iCurrentTime - iPreviousTime));
-SDL_Delay((1000 / KR_FPS) - (iCurrentTime - iPreviousTime));
-
-//iPreviousTime = iCurrentTime;
-}
-*/
