1e626755d586426c9a585adf8f5021017584b4e2
Oherrou <olivier.herrou@insa-rennes.fr>
2015-05-06 18:20:57 +0200

    Développement bouton (1)

--- Menu.c
+++ Menu.c
@@ -24,444 +24,171 @@
 /*	  				      FONCTIONS POUR LES BOUTONS  	   					 */
 /*===========================================================================*/
 
-
-
-/*
-
-
-
 /*!
-*  \fn      Bouton * Bouton_Init(char *BoutonName)
+*  \fn      Bouton * Bouton_Init(char *BoutonName, SDL_Renderer *pRenderer, char* szImageActive, char *szImageDesactive, char *szSelect)
 *  \brief   Function to initiate a button
 *
-*  \param   BoutonName    name of the button structure
+*  \Remarks the size of all the image (activate, desactivate, selection) must be the same ! there's no control for it
 *
+*  \param   BoutonName			name of the button structure
+*  \param   pRenderer			a pointer to the renderer
+*  \param   pTextureImage		the name of the image of the button
+*  \param   szImageActive		the name of the image of the button when it's activated
+*  \param   szImageDesactive	the name of the image of the button when it's desactivated
+*  \param   szSelect			the name of the image of the button when the mouse cursor is above it
 *  \return  the initialized structure
 */
-/*
-Bouton * Bouton_Init(char *BoutonName)
+Bouton *Bouton_Init(char *BoutonName, SDL_Renderer *pRenderer, char* szImageActive, char *szImageDesactive, char *szSelect)
 {
-	Bouton * pBouton = NULL;
-	pBouton = (Bouton *)UTIL_Malloc(sizeof(Bouton));
+	Bouton	  *pBouton = NULL;
+	Uint32     iNameLen = strlen(BoutonName);
+	char szPath[60] = "";
 
-	pBouton->BoutonName = BoutonName;
+	pBouton = (Bouton *)UTIL_Malloc(sizeof(Bouton));	
 
-	pBouton->pTextureImage = NULL;
-	pBouton->pTextureTexte = NULL;
+	pBouton->szBoutonName = UTIL_CopyStr(BoutonName, iNameLen);
 
-	pBouton->RectImage.x = 0;
-	pBouton->RectImage.y = 0;
-	pBouton->RectImage.w = 0;
-	pBouton->RectImage.h = 0;
+	sprintf(szPath, "menu/%s.png", szImageActive);
+	pBouton->pTextureImageActive = UTIL_LoadTexture(pRenderer, szPath, NULL, &(pBouton->RectImage));
+	if (pBouton->pTextureImageActive == NULL) return pBouton = NULL;
 
-	pBouton->RectTexte.x = 0;
-	pBouton->RectTexte.y = 0;
-	pBouton->RectTexte.w = 0;
-	pBouton->RectTexte.h = 0;
+	sprintf(szPath, "menu/%s.png", szImageDesactive);
+	pBouton->pTextureImageDesactive = UTIL_LoadTexture(pRenderer, szPath, NULL, NULL);
+	if (pBouton->pTextureImageDesactive == NULL) return pBouton = NULL;
 
-	pBouton->TexteActive = NULL;
-	pBouton->TexteNonActive = NULL;
+	sprintf(szPath, "menu/%s.png", szSelect);
+	pBouton->pTextureSelection = UTIL_LoadTexture(pRenderer, szPath, NULL, NULL);
+	if (pBouton->pTextureSelection == NULL) return pBouton = NULL;
 
-	pBouton->estSurvole = FALSE;
-	pBouton->estActive = FALSE;
-	pBouton->AffichageOK = FALSE;
-
+	pBouton->pRenderer = pRenderer;
+	pBouton->pTextureActive = NULL;
+	pBouton->pTextureDesactive = NULL;
+	pBouton->RectTexteActive.x = 0;
+	pBouton->RectTexteActive.y = 0;
+	pBouton->RectTexteActive.w = 0;
+	pBouton->RectTexteActive.h = 0;
+	pBouton->RectTexteDesactive.x = 0;
+	pBouton->RectTexteDesactive.y = 0;
+	pBouton->RectTexteDesactive.w = 0;
+	pBouton->RectTexteDesactive.h = 0;
+	pBouton->bSurvole = FALSE;
+	pBouton->bActive = FALSE;
+	pBouton->bMustShow = FALSE;
 	return pBouton;
 }
-*/
-
 
 
 /*!
-*  \fn      void Bouton_Load(SDL_Renderer * renderer, Bouton *pBouton, TTF_Font *pFont, SDL_Color Color, SDL_Rect RectImage)
-*  \brief   Function to initiate a Bouton
+*  \fn      void Bouton_Free(Bouton *pBouton)
+*  \brief   Function to free a button structure
 *
-*  \param   *renderer   the renderer to display the button
-*  \param   *pBouton    the Bouton structure
-*  \param   *pFont	    the font of the text of the button
-*  \param   Color       the color of the text of the button 
-*  \param   RectImage   the rectngle of the backgrund of the button
-*
-*  \return  void
+*  \param   pBouton  the structure to free
+*  \return  none
 */
-/*
-void Bouton_Load(SDL_Renderer * renderer, Bouton *pBouton, TTF_Font *pFont, SDL_Color Color, SDL_Rect RectImage, char* TexteActive, char* TexteNonActive)
+void Bouton_Free(Bouton *pBouton)
 {
-	SDL_Rect RectTexteTampon = { 0, 0, 0, 0 };
+	UTIL_FreeTexture(&pBouton->pTextureImageActive);
+	UTIL_FreeTexture(&pBouton->pTextureImageDesactive);
+	UTIL_FreeTexture(&pBouton->pTextureSelection);
+	UTIL_FreeTexture(&pBouton->pTextureActive);
+	UTIL_FreeTexture(&pBouton->pTextureDesactive);
+	UTIL_Free(pBouton);
+}
+
+
+/*!
+*  \fn     Boolean Bouton_Load(Bouton *pBouton, Boolean bMustShow, TTF_Font *pFont, SDL_Color Color, SDL_Rect RectImage, char* TexteActive, char* TexteNonActive)
+*  \brief  Function to load a bouton structure
+*
+*  \param   pBouton			the bouton structure
+*  \param   bMustShow		TRUE to display the button
+*  \param   Color			the font
+*  \param   RectImage		the position of the button
+*  \param   TexteActive		the string to display when the button is activated
+*  \param   TexteNonActive	the string to display when the button is not activated
+*  \return  TRUE if everything is ok, FALSE otherwise
+*/
+Boolean Bouton_Load(Bouton *pBouton, Boolean bMustShow, TTF_Font *pFont, SDL_Color Color, SDL_Rect RectImage, char* TexteActive, char* TexteNonActive)
+{
+	pBouton->bMustShow = bMustShow;
 
 	pBouton->RectImage.x = RectImage.x;
 	pBouton->RectImage.y = RectImage.y;
-	pBouton->RectImage.w = RectImage.w;
-	pBouton->RectImage.h = RectImage.h;
 
-	pBouton->TexteActive = TexteActive;
-	pBouton->TexteNonActive = TexteNonActive;
+	UTIL_FreeTexture(&pBouton->pTextureActive);
+	UTIL_FreeTexture(&pBouton->pTextureDesactive);
 
-	char BoutonPath[50];
-	sprintf(BoutonPath, "Bouton\\%s.png", "Fond_Bouton");
-	pBouton->pTextureImage = UTIL_LoadTexture(BoutonPath, NULL, NULL);
-	if (pBouton->pTextureImage == NULL)
+	// Activé
+	pBouton->pTextureActive = Kr_Text_FontCreateTexture(pBouton->pRenderer, pFont, TexteActive, Color, TRUE, &pBouton->RectTexteActive);
+	if (pBouton->pTextureActive == NULL)
 	{
-		Kr_Log_Print(KR_LOG_ERROR, "Impossible to load the sprite :%s\n", BoutonPath);
+		Kr_Log_Print(KR_LOG_WARNING, "Can't update the text of the button structure (activate)!\n");
+		return FALSE;
 	}
-
-	pBouton->pTextureTexte = Kr_Text_FontCreateTexture(renderer, pFont, pBouton->TexteActive, Color, TRUE, &RectTexteTampon);
-
-	pBouton->RectTexte.x = pBouton->RectImage.x + ((pBouton->RectImage.w - RectTexteTampon.w) / 2);
-	pBouton->RectTexte.y = pBouton->RectImage.y + ((pBouton->RectImage.h - RectTexteTampon.h) / 2);
-	pBouton->RectTexte.w = RectTexteTampon.w;
-	pBouton->RectTexte.h = RectTexteTampon.h;
-
-	pBouton->AffichageOK = TRUE;
-	pBouton->estActive = TRUE;
-}*/
-
-
-
-
-/*!
-*  \fn      void Bouton_Draw(SDL_Renderer * renderer, Bouton *pBouton)
-*  \brief   Function to display the Bouton
-*
-*  \param   *renderer      the screen to display the Button
-*  \param   *pBouton       the Bouton structure
-*  \param   *pFont         the font used to create the texture of the button
-*  \param   pBouton        the color of the font
-*
-*  \return  void
-*/
-/*
-void Bouton_Draw(SDL_Renderer * renderer, Bouton *pBouton)
-{
-	// Test pour savoir si le bouton peut être affiché 
-	if (pBouton->AffichageOK == TRUE)
+	pBouton->RectTexteActive.x = pBouton->RectImage.x + ((pBouton->RectImage.w - pBouton->RectTexteActive.w) / 2);
+	pBouton->RectTexteActive.y = pBouton->RectImage.y + ((pBouton->RectImage.h - pBouton->RectTexteActive.h) / 2);
+	
+	// Désactivé
+	pBouton->pTextureDesactive = Kr_Text_FontCreateTexture(pBouton->pRenderer, pFont, TexteNonActive, Color, TRUE, &pBouton->RectTexteDesactive);
+	if (pBouton->pTextureDesactive == NULL)
 	{
-		// Affichage de l'image de fond du bouton
-		SDL_RenderCopy(renderer, pBouton->pTextureImage, NULL, &(pBouton->RectImage));
-		// Affichage de la texture texte 
-		SDL_RenderCopy(renderer, pBouton->pTextureTexte, NULL, &(pBouton->RectTexte));
-		return;
+		Kr_Log_Print(KR_LOG_WARNING, "Can't update the text of the button structure (desactivate)!\n");
+		return FALSE;
 	}
-	else{ return; }
+	pBouton->RectTexteDesactive.x = pBouton->RectImage.x + ((pBouton->RectImage.w - pBouton->RectTexteDesactive.w) / 2);
+	pBouton->RectTexteDesactive.y = pBouton->RectImage.y + ((pBouton->RectImage.h - pBouton->RectTexteDesactive.h) / 2);
+
+	return TRUE;
 }
 
-*/
 
-
-/*!
-*  \fn      void Bouton_free(Bouton *pBouton)
-*  \brief   Function to free the Bouton
-*
-*  \param   *pBouton      the bouton structure
-*
-*  \return  void
-*/
-/*
-void Bouton_free(Bouton *pBouton)
-{
-	UTIL_FreeTexture(&(pBouton->pTextureImage));
-	UTIL_FreeTexture(&(pBouton->pTextureTexte));
-	UTIL_Free(pBouton);
-}
-*/
 
 
 /*!
-*  \fn      void Bouton_UpdateImage(Bouton *pBouton)
-*  \brief   Function to update the texture of the background of the button
+*  \fn     void Bouton_Draw(Bouton *pBouton)
+*  \brief  Function to draw a button
 *
-*  \param   *pBouton      the button structure
-*
-*  \return  void
+*  \param   pBouton	the bouton structure
+*  \return none
 */
-/*
-void Bouton_UpdateImage(Bouton *pBouton)
+void Bouton_Draw(Bouton *pBouton)
 {
-	char BoutonPath[50];
-	if (pBouton->estSurvole == TRUE)
+	if (!pBouton->bMustShow) return;
+
+	if (pBouton->bActive)
 	{
-		UTIL_FreeTexture(&(pBouton->pTextureImage));
-		sprintf(BoutonPath, "Bouton\\%s.png", "Fond_Bouton_Selectionne");
-		pBouton->pTextureImage = UTIL_LoadTexture(BoutonPath, NULL, NULL);
-		if (pBouton->pTextureImage == NULL)
-		{
-			Kr_Log_Print(KR_LOG_ERROR, "Impossible to load the sprite :%s\n", BoutonPath);
-		}
-		return;
+		SDL_RenderCopy(pBouton->pRenderer, pBouton->pTextureImageActive, NULL, &pBouton->RectImage);
+		SDL_RenderCopy(pBouton->pRenderer, pBouton->pTextureActive, NULL, &pBouton->RectTexteActive);
 	}
 	else
 	{
-		UTIL_FreeTexture(&(pBouton->pTextureImage));
-		sprintf(BoutonPath, "Bouton\\%s.png", "Fond_Bouton");
-		pBouton->pTextureImage = UTIL_LoadTexture(BoutonPath, NULL, NULL);
-		if (pBouton->pTextureImage == NULL)
-		{
-			Kr_Log_Print(KR_LOG_ERROR, "Impossible to load the sprite :%s\n", BoutonPath);
-		}
-		return;
+		SDL_RenderCopy(pBouton->pRenderer, pBouton->pTextureImageDesactive, NULL, &pBouton->RectImage);
+		SDL_RenderCopy(pBouton->pRenderer, pBouton->pTextureDesactive, NULL, &pBouton->RectTexteDesactive);
 	}
-}*/
-
-
+}
 
 /*!
-*  \fn      void Bouton_ChangeEtatSurvol(Bouton *pBouton, Boolean Survole)
-*  \brief   Function to change the statute of estSurvole
+*  \fn     Boolean Bouton_estSurvole(Bouton *pBouton, Kr_Input myEvent)
+*  \brief  Function to draw a button
 *
-*  \param   *pBouton      the bouton structure
-*  \param   Survole		  Boolean telling if the mouse is on the button
-*
-*  \return  void
+*  \param   pBouton	the bouton structure
+*  \param   myEvent	the Kr_Input structure to get access to the coordinate of the mouse
+*  \return none
 */
-
-//void Bouton_ChangeEtatSurvol(Bouton *pBouton, Boolean Survole)
-//{
-//	pBouton->estSurvole = Survole;
-//}
-
-/*
 Boolean Bouton_estSurvole(Bouton *pBouton, Kr_Input myEvent)
 {
-	if ((pBouton->RectImage.x <= myEvent.iMouseX) &&
-		(myEvent.iMouseX <= (pBouton->RectImage.x + pBouton->RectImage.w)) &&
-		(pBouton->RectImage.y <= myEvent.iMouseY) &&
-		myEvent.iMouseY <= (pBouton->RectImage.y + pBouton->RectImage.h))
+	if (!pBouton->bMustShow) return FALSE; // Pas de vérification si le bouton n'est pas affiché
+
+	if (((Uint32)(pBouton->RectImage.x) <= myEvent.iMouseX) && (myEvent.iMouseX <= (Uint32)(pBouton->RectImage.x + pBouton->RectImage.w)) &&
+		((Uint32)(pBouton->RectImage.y) <= myEvent.iMouseY) && (myEvent.iMouseY <= (Uint32)(pBouton->RectImage.y + pBouton->RectImage.h)))
 	{
-		pBouton->estSurvole = TRUE;
-		Bouton_UpdateImage(pBouton);
+		pBouton->bSurvole = TRUE;
+		SDL_RenderCopy(pBouton->pRenderer, pBouton->pTextureSelection, NULL, &pBouton->RectImage);
 		return TRUE;
 	}
 	else
 	{
-		pBouton->estSurvole = FALSE;
-		Bouton_UpdateImage(pBouton);
+		pBouton->bSurvole = FALSE;
 		return FALSE;
 	}
-}*/
-
-
-
-
-/*!
-*  \fn      void Bouton_estActive(Bouton *pBouton, Boolean Survole)
-*  \brief   Function to change the statute of estSelectionne into TRUE
-*
-*  \param   *pBouton      the bouton structure
-*  \param   Active		  Boolean telling if the mouse is on the button
-*
-*  \return  void
-*/
-/*
-void Bouton_estActive(Bouton *pBouton)
-{
-	if (pBouton->estActive == TRUE)
-	{
-		pBouton->estActive = FALSE;
-	}
-	else
-	{
-		pBouton->estActive = TRUE;
-	}
-}*/
-
-
-
-
-/*!
-*  \fn      void Bouton_UpdateImage(Bouton *pBouton)
-*  \brief   Function to update the texture of the background of the button
-*
-*  \param   *pBouton      the button structure
-*
-*  \return  void
-*/
-/*
-void Bouton_UpdateTexte(SDL_Renderer * renderer, Bouton *pBouton, TTF_Font *pFont, SDL_Color Color)
-{
-	SDL_Rect RectTexteTampon = { 0, 0, 0, 0 };
-	if (pBouton->estActive == TRUE)
-	{
-		UTIL_FreeTexture(&(pBouton->pTextureTexte));
-		pBouton->pTextureTexte = Kr_Text_FontCreateTexture(renderer, pFont, pBouton->TexteActive, Color, TRUE, &RectTexteTampon);
-		/* Calcul des nouveaux coordonnes de RectTexte */
-/*		pBouton->RectTexte.x = pBouton->RectImage.x + ((pBouton->RectImage.w - RectTexteTampon.w) / 2);
-		pBouton->RectTexte.y = pBouton->RectImage.y + ((pBouton->RectImage.h - RectTexteTampon.h) / 2);
-		pBouton->RectTexte.w = RectTexteTampon.w;
-		pBouton->RectTexte.h = RectTexteTampon.h;
-		return;
-	}
-	else
-	{
-		UTIL_FreeTexture(&(pBouton->pTextureTexte));
-		pBouton->pTextureTexte = Kr_Text_FontCreateTexture(renderer, pFont, pBouton->TexteNonActive, Color, TRUE, &RectTexteTampon);
-		/* Calcul des nouveaux coordonnes de RectTexte */
-/*		pBouton->RectTexte.x = pBouton->RectImage.x + ((pBouton->RectImage.w - RectTexteTampon.w) / 2);
-		pBouton->RectTexte.y = pBouton->RectImage.y + ((pBouton->RectImage.h - RectTexteTampon.h) / 2);
-		pBouton->RectTexte.w = RectTexteTampon.w;
-		pBouton->RectTexte.h = RectTexteTampon.h;
-		return;
-	}
-}*/
-
-
-
-
-/*===========================================================================*/
-/*	  				      FONCTIONS POUR LES MENUS  	   					 */
-/*===========================================================================*/
-
-
-/*!
-*  \fn      Menu * Menu_Init(char *MenuName, Uint32 NbBouton)
-*  \brief   Function to initiate a Menu
-*
-*  \param   MenuName    name of the Menu
-*  \param   NbBouton	Number of button persent in the Menu
-*
-*  \return  the initialized structure
-*/
-/*
-Menu * Menu_Init(char *MenuName)
-{
-	Menu * pMenu = NULL;
-	pMenu = (Menu *)UTIL_Malloc(sizeof(Menu));
-
-	pMenu->MenuName = MenuName;
-
-	pMenu->RectFond.x = 0;
-	pMenu->RectFond.y = 0;
-	pMenu->RectFond.w = 0;
-	pMenu->RectFond.h = 0;
-
-	//pMenu->NbBouton = NbBouton;
-
-	pMenu->pTextureFond = NULL;
-
-	pMenu->pBouton1 = NULL;
-	pMenu->pBouton2 = NULL;
-	pMenu->pBouton3 = NULL;
-	pMenu->pBouton4 = NULL;
-	pMenu->pBouton5 = NULL;
-
-
-	//pMenu->TabBouton = malloc(NbBouton * sizeof(Bouton));
-
-	return pMenu;
-}*/
-
-
-
-
-/*!
-*  \fn      void Menu_Load(Menu *pMenu, ...)
-*  \brief   Function to load a Menu
-*
-*  \param   *pMenu    a menu structure
-*
-*  \return  void
-*/
-
-/*void Menu_Load(Menu *pMenu, Uint32 NbBouton, ...)*/
-/*
-void Menu_Load(Menu *pMenu, Bouton *pBouton1, Bouton *pBouton2, Bouton *pBouton3, Bouton *pBouton4, Bouton *pBouton5)
-{
-	//Remplissage de TabBouton ?????
-
-	//Uint32 i = 0;
-/*	Bouton parametre;
-	va_list ap;
-	va_start(ap, NbBouton);
-	for (i = 0; i < NbBouton; i++)
-	{ 
-		pMenu->TabBouton[i] = parametre;
-		parametre = va_arg(ap, Bouton);
-	}
-	va_end(ap);
-*/
-/*
-//	pMenu->TabBouton[0] = pBouton0;
-//	pMenu->TabBouton = TabMenu;
-
-	pMenu->RectFond.w = 1280;
-	pMenu->RectFond.h = 704;
-
-	pMenu->pBouton1 = pBouton1;
-	pMenu->pBouton2 = pBouton2;
-	pMenu->pBouton3 = pBouton3;
-	pMenu->pBouton4 = pBouton4;
-	pMenu->pBouton5 = pBouton5;
-
-	char MenuPath[50];
-	sprintf(MenuPath, "Bouton\\%s.png", "Fond_Menu");
-	pMenu->pTextureFond = UTIL_LoadTexture(MenuPath, NULL, NULL);
-	if (pMenu->pTextureFond == NULL)
-	{
-		Kr_Log_Print(KR_LOG_ERROR, "Impossible to load the sprite :%s\n", MenuPath);
-	}
 }
-
-
-*/
-
-/*!
-*  \fn      void Menu_free(Menu *pMenu)
-*  \brief   Function to free the menu
-*
-*  \param   *pMenu      the Menu structure
-*
-*  \return  void
-*/
-/*
-void Menu_free(Menu *pMenu)
-{
-	// Ca ou Free(pMenu->TabBouton)     ???
-	//Uint32 i = 0;
-/*	for (i = 0; i < pMenu->NbBouton; i++)
-	{
-		Bouton_free(&(pMenu->TabBouton[i]));
-	}
-*/
-	//Bouton_free(pMenu->pBouton1);
-	//Bouton_free(pMenu->pBouton2);
-	//Bouton_free(pMenu->pBouton3);
-	//Bouton_free(pMenu->pBouton4);
-	//Bouton_free(pMenu->pBouton5);
-/*
-	UTIL_FreeTexture(&(pMenu->pTextureFond));
-	UTIL_Free(pMenu);
-}*/
-
-
-
-
-/*!
-*  \fn      void Menu_Draw(SDL_Renderer * renderer, Menu *pMenu)
-*  \brief   Function to display the Menu
-*
-*  \param   *renderer      the screen to display the Menu
-*  \param   *pMenu	       the Menu structure
-*
-*  \return  void
-*/
-/*
-void Menu_Draw(SDL_Renderer * renderer, Menu *pMenu)
-{
-	//SDL_RenderClear(renderer);
-//	Uint32 i = 0;
-	/* Affichage de l'image de fond du Menu */
-/*SDL_RenderCopy(renderer, pMenu->pTextureFond, NULL, &(pMenu->RectFond));*/
-	/* Affichage des boutons */
-/*	for (i = 0; i < pMenu->NbBouton; i++)
-	{
-		Bouton_Draw(renderer, &(pMenu->TabBouton[i]));
-	}
-*//*
-	Bouton_Draw(renderer, pMenu->pBouton1);
-	Bouton_Draw(renderer, pMenu->pBouton2);
-	Bouton_Draw(renderer, pMenu->pBouton3);
-	Bouton_Draw(renderer, pMenu->pBouton4);
-	Bouton_Draw(renderer, pMenu->pBouton5);
-	return;
-}
-*/
-
--- Menu.h
+++ Menu.h
@@ -12,6 +12,7 @@
 /* Guillaume     | 22/03/2015 | Création                                         */
 /* Guillaume     | 23/03/2015 | Modification struture - ajout image de fond      */
 /*													  - ajout booleen selection  */
+/* Herrou        | 06/05/2015 | Modification                                     */
 /*																			     */
 /* ============================================================================= */
 
@@ -32,17 +33,21 @@
 /*	  				      STRUCTURE POUR LES BOUTONS  	   					 */
 /*===========================================================================*/
 
-typedef struct {
-	char*		BoutonName;		    /*!< Name of the button */
-	SDL_Rect	RectImage;			/*!< Rectangle of th background of the button */
-	SDL_Rect    RectTexte;			/*!< Rectangle of the text of the button*/
-	SDL_Texture *pTextureImage;		/*!< Texture of the background of the button */
-	SDL_Texture *pTextureTexte;		/*!< Texture of the text of the button */
-	char*       TexteActive;		/*!< Text to write on the button when it's validated */
-	char*       TexteNonActive;		/*!< Text to write on the button when it's validated */
-	Boolean     estSurvole;         /*!< Boolean telling if the button is selected */
-	Boolean     estActive;          /*!< Boolean telling if the button is validated */
-	Boolean		AffichageOK;		/*!< Boolean telling if the button can be displayed */
+typedef struct
+{
+	SDL_Renderer   *pRenderer;				/*!< A pointer to the renderer */
+	char		   *szBoutonName;			/*!< Name of the button */
+	SDL_Rect		RectImage;				/*!< Rectangle of the background of the button */
+	SDL_Rect		RectTexteActive;		/*!< Rectangle of the text of the button when it's activated */
+	SDL_Rect		RectTexteDesactive;		/*!< Rectangle of the text of the button when it's desactivated */
+	SDL_Texture	   *pTextureImageActive;	/*!< Texture of the background of the button when it's activated */
+	SDL_Texture	   *pTextureImageDesactive;	/*!< Texture of the background of the button when it's not activated */
+	SDL_Texture	   *pTextureSelection;		/*!< Texture to apply above the button when the mouse cursor is on it */
+	SDL_Texture	   *pTextureActive;			/*!< Texture of the text of the button when it's activated */
+	SDL_Texture	   *pTextureDesactive;		/*!< Texture of the text of the button when it's desactivated */
+	Boolean			bSurvole;				/*!< Boolean telling if the button is selected */
+	Boolean			bActive;				/*!< Boolean telling if the button is validated */
+	Boolean			bMustShow;				/*!< Boolean telling if the button can be displayed */
 } Bouton;
 
 
@@ -53,50 +58,11 @@ typedef struct {
 /*===========================================================================*/
 
 /* Fonctions pour la structure bouton */
-Bouton * Bouton_Init(char *BoutonName);
-void Bouton_Load(SDL_Renderer * renderer, Bouton *pBouton, TTF_Font *pFont, SDL_Color Color, SDL_Rect RectImage, char* TexteActive, char* TexteNonActive);
-void Bouton_Draw(SDL_Renderer * renderer, Bouton *pBouton);
-void Bouton_free(Bouton *pBouton);
-void Bouton_UpdateImage(Bouton *pBouton);
+Bouton *Bouton_Init(char *szBoutonName, SDL_Renderer *pRenderer, char* szImageActive, char *szImageDesactive, char *szSelect);
+Boolean	Bouton_Load(Bouton *pBouton, Boolean bMustShow, TTF_Font *pFont, SDL_Color Color, SDL_Rect RectImage, char* TexteActive, char* TexteNonActive);
+void	Bouton_Draw(Bouton *pBouton);
+void	Bouton_Free(Bouton *pBouton);
 Boolean Bouton_estSurvole(Bouton *pBouton, Kr_Input myEvent);
-void Bouton_estActive(Bouton *pBouton);
-void Bouton_UpdateTexte(SDL_Renderer * renderer, Bouton *pBouton, TTF_Font *pFont, SDL_Color Color);
-
-
-
-
-
-/*===========================================================================*/
-/*	  				      STRUCTURE POUR LES MENUS  	   					 */
-/*===========================================================================*/
-
-typedef struct {
-	char*		 MenuName;		    /*!< Name of the menu */
-	SDL_Rect	 RectFond;			/*!< Rectangle of th background of the menu */
-	SDL_Texture  *pTextureFond;		/*!< texture of the background of the menu */
-	//Uint32		 NbBouton;			/*!< Number of button present in this menu */
-	//Bouton*		 TabBouton;			/*!< Array of the buttons displayed on the menu */
-	Bouton       *pBouton1;
-	Bouton       *pBouton2;
-	Bouton       *pBouton3;
-	Bouton       *pBouton4;
-	Bouton       *pBouton5;
-} Menu;
-
-
-
-
-/*===========================================================================*/
-/*	  				      FONCTIONS POUR LES MENUS  	   					 */
-/*===========================================================================*/
-
-Menu * Menu_Init(char *MenuName);
-//void Menu_Load(Menu *pMenu, Uint32 NbBouton, ...);
-//void Menu_Load(Menu *pMenu, Menu* TabMenu);
-void Menu_Load(Menu *pMenu, Bouton *pBouton1, Bouton *pBouton2, Bouton *pBouton3, Bouton *pBouton4, Bouton *pBouton5);
-void Menu_free(Menu *pMenu);
-void Menu_Draw(SDL_Renderer * renderer, Menu *pMenu);
-
 
 
 
